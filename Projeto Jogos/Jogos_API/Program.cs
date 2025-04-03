68bd6c635c8a5c33aa68298219738e001629f0d8
Isadora Macedo <isa22queiroz@gmail.com>
2025-04-03 16:55:39 -0300

    feat: API 100% finalizada

--- Projeto Jogos/Jogos_API/Program.cs
+++ Projeto Jogos/Jogos_API/Program.cs
@@ -1,43 +1,19 @@
-using System.Reflection;
-using Azure;
-using Jogos_API.Context;
+ï»¿using Jogos_API.Context;
 using Jogos_API.Interface;
 using Jogos_API.Repositories;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
-// Configuração do Azure Content Safety sem variáveis de ambiente
-//var endpoint = "https://moderatorservice.cognitiveservices.azure.com/";
-//var apiKey = "6F59xOioGcV9aN1jnUL2LXe9eTGO32E5J8rNeSoZ235FMvzZ86F0JQQJ99BDACYeBjFXJ3w3AAAHACOGpfCS";
-//var client = new ContentSafetyClient(new Uri(endpoint), new AzureKeyCredential(apiKey));
-//builder.Services.AddSingleton(client);
-
-// Configuração do Azure Content Safety com variáveis de ambiente
-var endpoint = builder.Configuration["AzureContentSafety:Endpoint"];
-var apiKey = builder.Configuration["AzureContentSafety:ApiKey"];
-
-
-if (string.IsNullOrEmpty(endpoint) || string.IsNullOrEmpty(apiKey))
-{
-    throw new InvalidOperationException("Azure Content Safety: Endpoint ou API Key não foram configurados.");
-}
-
-
-
-builder.Services // Acessa a coleção de serviços da aplicação (Dependency Injection)
+builder.Services // Acessa a coleÃ§Ã£o de serviÃ§os da aplicaÃ§Ã£o (Dependency Injection)
     .AddControllers() // Adiciona suporte a controladores na API (MVC ou Web API)
-    .AddJsonOptions(options => // Configura as opções do serializador JSON padrão (System.Text.Json)
+    .AddJsonOptions(options => // Configura as opÃ§Ãµes do serializador JSON padrÃ£o (System.Text.Json)
     {
-        // Configuração para ignorar propriedades nulas ao serializar objetos em JSON
+        // ConfiguraÃ§Ã£o para ignorar propriedades nulas ao serializar objetos em JSON
         options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
 
-        // Configuração para evitar referência circular ao serializar objetos que possuem relacionamentos recursivos
+        // ConfiguraÃ§Ã£o para evitar referÃªncia circular ao serializar objetos que possuem relacionamentos recursivos
         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
     });
 
@@ -45,18 +21,13 @@ builder.Services // Acessa a coleção de serviços da aplicação (Dependency Inject
 builder.Services.AddDbContext<JogosDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//Adiciona o repositório e a interface ao container de injecao de dependência
-builder.Services.AddScoped<IJogosRepository, JogosRepository>();
+// Adicionar o  e a interface ao container de injeÃ§Ã£o de dependÃªncia
 builder.Services.AddScoped<IUsuariosRepository, UsuariosRepository>();
+builder.Services.AddScoped<IJogosRepository, JogosRepository>();
 
-
-//Adiciona o serviço de Controllers
+//Adicionar o serviÃ§o de Controllers
 builder.Services.AddControllers();
 
-
-
-
-//Adiciona Swagger
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(options =>
@@ -64,13 +35,13 @@ builder.Services.AddSwaggerGen(options =>
     options.SwaggerDoc("v1", new OpenApiInfo
     {
         Version = "v1",
-        Title = "API de Eventos",
-        Description = "Aplicação para gerenciamento de eventos",
+        Title = "API de Games",
+        Description = "AplicaÃ§ao para gerenciamento de Games",
         TermsOfService = new Uri("https://example.com/terms"),
         Contact = new OpenApiContact
         {
-            Name = "Carlos Roque",
-            Url = new Uri("https://www.linkedin.com/in/roquecarlos/")
+            Name = "Clara Crastechini",
+            Url = new Uri("https://github.com/Clara-Crastechini")
         },
         License = new OpenApiLicense
         {
@@ -78,39 +49,9 @@ builder.Services.AddSwaggerGen(options =>
             Url = new Uri("https://example.com/license")
         }
     });
-
-    //Configura o Swagger para usar o arquivo XML gerado
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-
-    //Usando a autenticaçao no Swagger
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Value: Bearer TokenJWT ",
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
 });
 
-
-// Adiciona CORS
+// CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
@@ -123,22 +64,5 @@ builder.Services.AddCors(options =>
 });
 
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger(options =>
-    {
-        options.SerializeAsV2 = true;
-    });
-
-    app.UseSwagger(options => // UseSwaggerUI is called only in Development.
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        using Microsoft.AspNetCore.Builder;
-    });
-}
-
-//Adiciona o Cors(política criada)
-app.UseCors("CorsPolicy");
-
+var app = builder.Build();
 app.Run();
--- Projeto Jogos/Jogos_API/obj/Debug/net8.0/Jogos_API.AssemblyInfo.cs
+++ Projeto Jogos/Jogos_API/obj/Debug/net8.0/Jogos_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jogos_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6731c5d3b0208d2ef327aee80e7d6a2d9ad1fe4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jogos_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jogos_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
